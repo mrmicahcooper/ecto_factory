b049b4323487feb7742d7b6c8fc58c442397efa8
Micah Cooper <micah+github@mrmicahcooper.com>
2016-06-21 11:22:03 -0400

    Add logo to docs

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule EctoFactory.Mixfile do
       description: description,
       deps: deps,
       docs: [
+        logo: "./logos/ectofactory_logo.png",
         extras: [
           "README.md"
         ]
