61442108c026292edd3d872ae2f579f7b795d2e9
Micah Cooper <micah+github@mrmicahcooper.com>
2020-11-04 09:10:31 -0500

    Patch bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoFactory.Mixfile do
   def project do
     [
       app: :ecto_factory,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9.1",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
